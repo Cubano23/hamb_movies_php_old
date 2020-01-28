d3df3dc8570ace47186baa7603ae9b5aaa267592
soares <bruno-soares@legtux.org>
2020-01-28 18:46:27 +0100

    evolution affichage des films

--- application/www/js/services/getData.js
+++ application/www/js/services/getData.js
@@ -1,27 +1,43 @@
 
 
 
-  let newSmall = document.createElement("small");
-  let newImg = document.createElement("img");
 
+   let fragment = document.createDocumentFragment(),
+   newImg = document.createElement("img");
 
+   let fragment2 = document.createDocumentFragment(),
+   title = document.createElement("p");
 
    function performSearch(searchTerm){
+    document.getElementById('title-film').innerHTML = "";
+    document.getElementById('gallery').innerHTML = "";
    fetch("https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm)
 
     .then((response) => { response.json()
             .then((value) => {
+                fragment.appendChild(newImg);
+                fragment.appendChild(title);
                 for(let i = 0; i < value.results.length; i++){
-                    newImg.src="https://image.tmdb.org/t/p/w185" + value.results[i].poster_path;
-                
-                    console.log(value.results[i].poster_path);
-               
-                    document.getElementById('gallery').appendChild(newImg);
-               
+
+                    newImg.src = "https://image.tmdb.org/t/p/w185" + value.results[i].poster_path; 
+                    fragment.appendChild(newImg.cloneNode(true)); 
+                    
+                    title.innerHTML = value.results[i].title;
+                    fragment.appendChild(title.cloneNode(true)); 
+
+                            
                 }
+                document.getElementById('gallery').appendChild(fragment);
+                document.getElementById('title-film').appendChild(fragment);
+
+                    console.log(value.results.length);   
+                    console.log(value.results); 
+                             
                
              
             });
     });
+
 }
+
 
